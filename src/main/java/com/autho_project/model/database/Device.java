cff583d19fee9e00086bf730fab364ffe6928cbd
Nik Noss <nnossenko@gmail.com>
2023-09-10 12:48:23 +0300

    Added jdoc comments

--- src/main/java/com/autho_project/model/database/Device.java
+++ src/main/java/com/autho_project/model/database/Device.java
@@ -1,44 +1,65 @@
 package com.autho_project.model.database;
 
 /**
-* Author Nikita Nossenko
-* 
-* Device data entity
-*/
-
-import jakarta.persistence.*;
-
+ * Author: Nikita Nossenko
+ * 
+ * This class represents a Device entity that is stored in the database.
+ */
 @Entity
-@Table(name="device")
+@Table(name = "device")
 public class Device {
+    /**
+     * The unique identifier for the device.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "deviceID")
     private int deviceID;
 
+    /**
+     * Indicates whether the device is currently on or off.
+     */
     @Column(name = "on/off")
     private boolean onOff;
 
+    /**
+     * Indicates whether the device is set for automation.
+     */
     @Column(name = "automation")
     private boolean automation;
 
+    /**
+     * The usage data associated with the device.
+     */
     @Column(name = "usage")
     private Long usage;
 
+    /**
+     * The name of the device.
+     */
     @Column(name = "name")
     private String name;
 
+    /**
+     * The model code of the device.
+     */
     @Column(name = "model_code")
     private String modelCode;
 
-    /*@JoinColumn(name = "groupID")
-    private...
-
-    @JoinColumn(name = "typeID")
-    private...*/
-
+    /**
+     * Default constructor for creating a new Device instance.
+     */
     public Device() {}
 
+    /**
+     * Constructs a new Device instance with the specified parameters.
+     *
+     * @param onOff     Whether the device is on or off.
+     * @param automation    Whether the device is set for automation.
+     * @param usage     The usage data associated with the device.
+     * @param name      The name of the device.
+     * @param modelCode The model code of the device.
+     */
     public Device(boolean onOff, boolean automation, Long usage, String name, String modelCode) {
         this.onOff = onOff;
         this.automation = automation;
