dd7d4bc51267ee26ddbd892ae5a43a150ba81ded
elmooe <elmo.erla@live.fi>
2023-10-05 13:42:38 +0300

    roomsControlleria muutettu

--- src/main/java/com/automaatio/controller/mainpage/menu/RoomsController.java
+++ src/main/java/com/automaatio/controller/mainpage/menu/RoomsController.java
@@ -1,24 +1,14 @@
 package com.automaatio.controller.mainpage.menu;
 
 import com.automaatio.controller.mainpage.CreateRoomsVBoxColumn;
-import com.automaatio.controller.mainpage.CreateVBoxColumn;
 import com.automaatio.model.database.DeviceGroup;
-
 import com.automaatio.model.database.DeviceGroupDAO;
 import com.automaatio.utils.CacheSingleton;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -49,61 +39,18 @@ public class RoomsController implements Initializable, Menu {
         }
     }
 
-
-    //Load the new room FXML-file, clear the mainView and set the newView
-    @FXML
-    private void onShowRoomClick(DeviceGroup room) throws IOException {
-        cache.setRoom(room);                                //set the room in singleton
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/room.fxml"));
-            Parent newView = loader.load();
-            mainPane.getChildren().clear();
-            mainPane.getChildren().add(newView);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     @FXML
     private void onAddGroupClick() {
-        DeviceGroup newRoom = new DeviceGroup(newRoomTextField.getText(), cache.getUser());
-
-        newRoomTextField.setText("");                                   //clear the textfield
-        roomsVBox.getChildren().add(makeVBoxForNewRoom(newRoom));       //add the new room to VBox
-
-        // Show the room page
-        try {
-            onShowRoomClick(newRoom);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        String deviceGroupName = newRoomTextField.getText();
+        if (deviceGroupName == null || deviceGroupName.trim().isEmpty()) {
+            System.out.println("virhe nimeä antaessa");
+            return;
         }
-        System.out.println("Added group/room: " + newRoom.getName());
-
+        DeviceGroup newRoom = new DeviceGroup(newRoomTextField.getText(), cache.getUser());
         deviceGroupDAO.addObject(newRoom);
+        newRoomTextField.clear();
         System.out.println(newRoom);
-    }
-
-    private HBox makeVBoxForNewRoom(DeviceGroup room) {
-        HBox box = new HBox();
-        box.setSpacing(20);
-
-        Button showRoom = new Button("Open");
-        showRoom.setFont(Font.font("Copperplate Gothic Bold", 16.0));
-        showRoom.getProperties().put("room", room);
-        showRoom.setOnAction(event -> {
-            DeviceGroup savedRoom = (DeviceGroup) showRoom.getProperties().get("room");
-            try {
-                onShowRoomClick(savedRoom);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
-
-        Text title = new Text(room.getName());
-        title.setFont(Font.font("Copperplate Gothic Bold", 22.0));
-
-        box.getChildren().add(title);
-        box.getChildren().add(showRoom);
-        return box;
+        System.out.println("lisätty " + newRoom.getName() + " tietokantaan");
+        show();
     }
 }
